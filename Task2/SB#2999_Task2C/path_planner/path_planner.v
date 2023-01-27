7139e58cde044cd977b29a50a0374e50d854027c
Zain <beingzainsv@gmail.com>
2023-01-27 14:52:19 +0530

    Path Planning for hardware implemented

--- Task2/SB#2999_Task2C/path_planner/path_planner.v
+++ Task2/SB#2999_Task2C/path_planner/path_planner.v
@@ -37,7 +37,9 @@ module path_planner
 	input [4:0] s_node,
 	input [4:0] e_node,
 	output reg done,	
-	output reg [10*5-1:0] final_path
+	output reg [10*5-1:0] final_path,
+	output reg [10*2-1:0] direction,
+	output reg [5:0] direction_index
 );
 
 ////////////////////////WRITE YOUR CODE FROM HERE////////////////////
@@ -45,12 +47,15 @@ reg [4:0] i;
 reg [4:0] j;
 reg [4:0] temp;
 parameter [6:0] infinity = 7'b1111111; //infinity
-reg [4:0] cost_matrix[0:25][0:25]; //maze cost cost_matrix
+reg [1:0] cost_matrix[0:27][0:27][0:1]; //maze cost cost_matrix
 reg [12:0] dist_h [0:25]; //keep track of previous visited nodes
 reg [12:0] dist [0:25]; //keep track of current visited nodes
-reg [10*5-1:0] final_path_reg = 0; //{10{5'd27}};
+reg [10*5-1:0] final_path_reg = 0; //{10{{5'd27},{3'd1}}};
 parameter IDLE = 0, S0 = 1, S1 = 2, S2 = 3;
+reg [10*2-1:0] direction_reg = 0;
 
+
+//north = 0, east = 1, south = 2, west = 3;
 
 //feed the maze into the ROM
 initial begin
@@ -63,73 +68,75 @@ initial begin
 	end
 	
 
-	cost_matrix [0][1] = 3'd3 ;
-	cost_matrix [1][0] = 3'd3 ;
-	cost_matrix [1][2] = 3'd3 ;
-	cost_matrix [1][13] = 3'd3 ;
-	cost_matrix [2][1] = 3'd3 ;
-	cost_matrix [2][3] = 3'd1 ;
-	cost_matrix [2][5] = 3'd3 ;
-	cost_matrix [3][2] = 3'd1 ;
-	cost_matrix [4][6] = 3'd3 ;
-	cost_matrix [5][2] = 3'd3 ;
-	cost_matrix [5][6] = 3'd1 ;
-	cost_matrix [5][9] = 3'd2 ;
-	cost_matrix [6][4] = 3'd3 ;
-	cost_matrix [6][5] = 3'd1 ;
-	cost_matrix [6][16] = 3'd3 ;
-	cost_matrix [7][12] = 3'd2 ;
-	cost_matrix [8][9] = 3'd1 ;
-	cost_matrix [9][5] = 3'd2 ;
-	cost_matrix [9][8] = 3'd1 ;
-	cost_matrix [9][15] = 3'd1 ;
-	cost_matrix [10][16] = 3'd2 ;
-	cost_matrix [11][12] = 3'd3 ;
-	cost_matrix [12][7] = 3'd2 ;
-	cost_matrix [12][11] = 3'd3 ;
-	cost_matrix [12][13] = 3'd1 ;
-	cost_matrix [12][17] = 3'd3 ;
-	cost_matrix [13][1] = 3'd3 ;
-	cost_matrix [13][12] = 3'd1 ;
-	cost_matrix [13][18] = 3'd2 ;
-	cost_matrix [14][15] = 3'd1 ;
-	cost_matrix [15][9] = 3'd1 ;
-	cost_matrix [15][14] = 3'd1 ;
-	cost_matrix [15][22] = 3'd1 ;
-	cost_matrix [16][6] = 3'd3 ;
-	cost_matrix [16][10] = 3'd2 ;
-	cost_matrix [16][23] = 3'd2 ;
-	cost_matrix [17][12] = 3'd3 ;
-	cost_matrix [18][13] = 3'd2 ;
-	cost_matrix [18][19] = 3'd1 ;
-	cost_matrix [18][20] = 3'd1 ;
-	cost_matrix [19][18] = 3'd1 ;
-	cost_matrix [20][18] = 3'd1 ;
-	cost_matrix [20][21] = 3'd1 ;
-	cost_matrix [20][22] = 3'd2 ;
-	cost_matrix [21][20] = 3'd1 ;
-	cost_matrix [22][15] = 3'd1 ;
-	cost_matrix [22][20] = 3'd2;
-	cost_matrix [22][23] = 3'd1;
-	cost_matrix [22][25] = 3'd3;
-	cost_matrix [23][16] = 3'd2;
-	cost_matrix [23][22] = 3'd1;
-	cost_matrix [23][24] = 3'd2;
-	cost_matrix [24][23] = 3'd2;
-	cost_matrix [25][22] = 3'd3;
+	cost_matrix [0][1][0] = 3'd3 ; cost_matrix [0][1][1] = 3'd1;
+	cost_matrix [1][0][0] = 3'd3 ; cost_matrix [1][0][1] = 3'd1;
+	cost_matrix [1][2][0] = 3'd3 ; cost_matrix [1][2][1] = 3'd2;
+	cost_matrix [1][13][0] = 3'd3 ; cost_matrix [1][13][1] = 3'd1;
+	cost_matrix [2][1][0] = 3'd3 ; cost_matrix [2][1][1] = 3'd2;
+	cost_matrix [2][3][0] = 3'd1 ; cost_matrix [2][3][1] = 3'd1; 
+	cost_matrix [2][5][0] = 3'd3 ; cost_matrix [2][5][1] = 3'd2;
+	cost_matrix [3][2][0] = 3'd1 ; cost_matrix [3][2][1] = 3'd3;
+	cost_matrix [4][6][0] = 3'd3 ; cost_matrix [4][6][1] = 3'd0;
+	cost_matrix [5][2][0] = 3'd3 ; cost_matrix [5][2][1] = 3'd0;
+	cost_matrix [5][6][0] = 3'd1 ; cost_matrix [5][6][1] = 3'd2;
+	cost_matrix [5][9][0] = 3'd2 ; cost_matrix [5][9][1] = 3'd1; 
+	cost_matrix [6][4][0] = 3'd3 ; cost_matrix [6][4][1] = 3'd2;
+	cost_matrix [6][5][0] = 3'd1 ; cost_matrix [6][5][1] = 3'd0;
+	cost_matrix [6][16][0] = 3'd3 ; cost_matrix [6][16][1] = 3'd2; 
+	cost_matrix [7][12][0] = 3'd2 ; cost_matrix [7][12][1] = 3'd2;
+	cost_matrix [8][9][0] = 3'd1 ; cost_matrix [8][9][1] = 3'd2;
+	cost_matrix [9][5][0] = 3'd2 ; cost_matrix [9][5][1] = 3'd3;
+	cost_matrix [9][8][0] = 3'd1 ; cost_matrix [9][8][1] = 3'd0;
+	cost_matrix [9][15][0] = 3'd1 ; cost_matrix [9][15][1] = 3'd1;
+	cost_matrix [10][16][0] = 3'd2 ; cost_matrix [10][16][1] = 3'd2;
+	cost_matrix [11][12][0] = 3'd3 ; cost_matrix [11][12][1] = 3'd2;
+	cost_matrix [12][7][0] = 3'd2 ; cost_matrix [12][7][1] = 3'd3;
+	cost_matrix [12][11][0] = 3'd3 ; cost_matrix [12][11][1] = 3'd0;
+	cost_matrix [12][13][0] = 3'd1 ; cost_matrix [12][13][1] = 3'd2;
+	cost_matrix [12][17][0] = 3'd3 ; cost_matrix [12][17][1] = 3'd1;
+	cost_matrix [13][1][0] = 3'd3 ; cost_matrix [13][1][1] = 3'd3;
+	cost_matrix [13][12][0] = 3'd1 ; cost_matrix [13][12][1] = 3'd00 ;
+	cost_matrix [13][18][0] = 3'd2 ; cost_matrix [13][18][1] = 3'd1 ;
+	cost_matrix [14][15][0] = 3'd1 ; cost_matrix [14][15][1] = 3'd2;
+	cost_matrix [15][9][0] = 3'd1 ; cost_matrix [15][9][1] = 3'd3;
+	cost_matrix [15][14][0] = 3'd1 ; cost_matrix [15][14][1] = 3'd0 ;
+	cost_matrix [15][22][0] = 3'd1 ; cost_matrix [15][22][1] = 3'd1 ;
+	cost_matrix [16][6][0] = 3'd3 ; cost_matrix [16][6][1] = 3'd3;
+	cost_matrix [16][10][0] = 3'd2 ; cost_matrix [16][10][1] = 3'd0;
+	cost_matrix [16][23][0] = 3'd2 ; cost_matrix [16][23][1] = 3'd1;
+	cost_matrix [17][12][0] = 3'd3 ; cost_matrix [17][12][1] = 3'd3 ;
+	cost_matrix [18][13][0] = 3'd2 ; cost_matrix [18][13][1] = 3'd3 ;
+	cost_matrix [18][19][0] = 3'd1 ; cost_matrix [18][19][1] = 3'd1 ;
+	cost_matrix [18][20][0] = 3'd1 ; cost_matrix [18][20][1] = 3'd2 ;
+	cost_matrix [19][18][0] = 3'd1 ; cost_matrix [19][18][1] = 3'd3 ;
+	cost_matrix [20][18][0] = 3'd1 ; cost_matrix [20][18][1] = 3'd0 ;
+	cost_matrix [20][21][0] = 3'd1 ; cost_matrix [20][21][1] = 3'd1 ;
+	cost_matrix [20][22][0] = 3'd2 ; cost_matrix [20][22][1] = 3'd2 ;
+	cost_matrix [21][20][0] = 3'd1 ; cost_matrix [21][20][1] = 3'd3 ;
+	cost_matrix [22][15][0] = 3'd1 ; cost_matrix [22][15][1] = 3'd3 ;
+	cost_matrix [22][20][0] = 3'd2; cost_matrix [22][20][1] = 3'd0;
+	cost_matrix [22][23][0] = 3'd1; cost_matrix [22][23][1] = 3'd2;
+	cost_matrix [22][25][0] = 3'd3; cost_matrix [22][25][1] = 3'd1;
+	cost_matrix [23][16][0] = 3'd2; cost_matrix [23][16][1] = 3'd2;
+	cost_matrix [23][22][0] = 3'd1; cost_matrix [23][22][1] = 3'd0;
+	cost_matrix [23][24][0] = 3'd2; cost_matrix [23][24][1] = 3'd1;
+	cost_matrix [24][23][0] = 3'd2; cost_matrix [24][23][1] = 3'd3;
+	cost_matrix [25][22][0] = 3'd3; cost_matrix [25][22][1] = 3'd3; 
 	
 	for(i = 0;i<node_count-1;i=i+1) begin
-		cost_matrix[i][i] = 0;
+		cost_matrix[i][i][0] = 0; //fill all node_x -> node_x distances as 
+		cost_matrix[i][i][1] = 0;
+		cost_matrix[27][i][1] = 0;
 	end
 	
 end 
 
-reg [3:0] next_state = IDLE;
+reg [3:0] next_state = IDLE; //state variable
 reg [4:0] shortest_path_iter;
 reg [4:0] hold_update_mat_outing;
 reg [12:0] min = 0;
 reg [4:0] min_index = 0;
-reg [12:0] sel = {1'b1,{1'b0,1'b0,1'b0,1'b0,1'b0},{infinity}};
+reg [12:0] sel = {1'b1,{1'b0,1'b0,1'b0,1'b0,1'b0},{infinity}}; //selected node, {visited, node we came from, cost}
 reg [4:0] visited_count = 0;
 reg [1:0] readflag = 1;
 reg [3:0] count=0;
@@ -139,7 +146,7 @@ always @(posedge clk) begin
 
 	case(next_state)
 		IDLE: begin
-			if(start==1) begin
+			if(start==1) begin //when the start signal is given to the module, it starts the computation algorithm
 				done = 0;
 				visited_count =0;
 				j = s_node;
@@ -147,9 +154,11 @@ always @(posedge clk) begin
 				count = 1;
 				final_path_reg[4:0] = e_node;
 				sel = {1'b1,s_node,{infinity}};
+				direction_reg = 0;
 				next_state = S0;
 			end
             i = 0; 
+				//set previous as well as current visited nodes to zero
 				dist[0] = {1'b0,s_node[4:0],{infinity}};dist[1] = {1'b0,s_node[4:0],{infinity}};dist[2] = {1'b0,s_node[4:0],{infinity}};dist[3] = {1'b0,s_node[4:0],{infinity}};dist[4] = {1'b0,s_node[4:0],{infinity}};dist[5] = {1'b0,s_node[4:0],{infinity}};dist[6] = {1'b0,s_node[4:0],{infinity}};dist[7] = {1'b0,s_node[4:0],{infinity}};dist[8] = {1'b0,s_node[4:0],{infinity}};dist[9] = {1'b0,s_node[4:0],{infinity}};dist[10] = {1'b0,s_node[4:0],{infinity}};dist[11] = {1'b0,s_node[4:0],{infinity}};dist[12] = {1'b0,s_node[4:0],{infinity}};
 				dist[13] = {1'b0,s_node[4:0],{infinity}};dist[14] = {1'b0,s_node[4:0],{infinity}};dist[15] = {1'b0,s_node[4:0],{infinity}};dist[16] = {1'b0,s_node[4:0],{infinity}};dist[17] = {1'b0,s_node[4:0],{infinity}};dist[18] = {1'b0,s_node[4:0],{infinity}};dist[19] = {1'b0,s_node[4:0],{infinity}};dist[20] = {1'b0,s_node[4:0],{infinity}};dist[21] = {1'b0,s_node[4:0],{infinity}};dist[22] = {1'b0,s_node[4:0],{infinity}};dist[23] = {1'b0,s_node[4:0],{infinity}};dist[24] = {1'b0,s_node[4:0],{infinity}};dist[25] = {1'b0,s_node[4:0],{infinity}};
 				
@@ -163,7 +172,8 @@ always @(posedge clk) begin
 				next_state <= S1;
 				j=e_node;
 				dist_h[s_node][11:7] = 5'b11011;
-				final_path_reg[49:5] = {10{5'd27}};
+				final_path_reg[49:5] = {9{5'd27}};
+				direction_reg[1:0] = cost_matrix[dist_h[j][11:7]][j][1];
 				//$display("DONE");	
 			end
 			else if(i == node_count-2) begin         //check end of nodes
@@ -174,10 +184,10 @@ always @(posedge clk) begin
 				dist_h[0] = dist[0] ; dist_h[1] = dist[1] ; dist_h[2] = dist[2] ; dist_h[3] = dist[3] ; dist_h[4] = dist[4] ; dist_h[5] = dist[5] ; dist_h[6] = dist[6] ; dist_h[7] = dist[7] ; dist_h[8] = dist[8] ; dist_h[9] = dist[9] ; dist_h[10] = dist[10] ; dist_h[11] = dist[11] ; dist_h[12] = dist[12] ; dist_h[13] = dist[13] ; dist_h[14] = dist[14] ; dist_h[15] = dist[15] ; dist_h[16] = dist[16] ; dist_h[17] = dist[17] ; dist_h[18] = dist[18] ; dist_h[19] = dist[19] ; dist_h[20] = dist[20] ; dist_h[21] = dist[21] ; dist_h[22] = dist[22] ; dist_h[23] = dist[23] ; dist_h[24] = dist[24] ; dist_h[25] = dist[25] ;
 				j = min_index;   //next column node = minimum index
 				i = -1;          
-				sel =  {1'b1,{min_index[4:0]},{infinity}}; //selected node = minimum index marked as visited and with infinite distance
+				sel =  {1'b1,{min_index[4:0]},{infinity}}; //selected node = minimum index marked as visited and with infinite distance. We turn it infinite as we've chosen that path
 			end
-			else if ( ((cost_matrix[j][i] + min[6:0]) < dist_h[i][6:0]) && (dist_h[i][12] != 1) ) begin
-				dist[i] = {1'b0,j,{cost_matrix[j][i] + min[6:0]}};    //update node dstance if it is less than path(min)
+			else if ( ((cost_matrix[j][i][0] + min[6:0]) < dist_h[i][6:0]) && (dist_h[i][12] != 1) ) begin
+				dist[i] = {1'b0,j,{cost_matrix[j][i][0] + min[6:0]}};    //update node dstance if it is less than path(min)
 			end
 			else begin
 				dist[i] = dist_h[i];
@@ -203,6 +213,7 @@ always @(posedge clk) begin
 			else begin
 				//final_path_reg = (final_path_reg << 5) + dist_h[j][11:7];
 				final_path_reg[(5*count+4)-:5] = dist_h[j][11:7];
+				direction_reg[(2*count+1)-:2] = cost_matrix[dist_h[j][11:7]][j][1];
 				count = count+1;
 				j = dist_h[j][11:7];
 				//$display("j: %0d",j);
@@ -213,6 +224,8 @@ end
 
 always @(posedge start or posedge done) begin
 	final_path <= final_path_reg;
+	direction <= direction_reg;
+	direction_index <= (count-2)*2+1;
 end
 	
 
